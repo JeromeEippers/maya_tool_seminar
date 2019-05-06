a39b1945ba8ba433e86e41fce8b5e8e8b68d15d2
Jerome Eippers <jerome@eippers.be>
2019-05-06 16:08:43 +0200

    Update exporter.py

--- fbx_exporter/exporter.py
+++ fbx_exporter/exporter.py
@@ -1,5 +1,7 @@
 from PySide2 import QtWidgets, QtGui
 import maya.cmds as cmds
+import maya.mel as mel
+import os
 from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
 
 class AbstractPickerWidget(QtWidgets.QWidget):
@@ -70,9 +72,6 @@ class MyWindow(MayaQWidgetDockableMixin, QtWidgets.QWidget):
         self.rootFolder = FolderPickerWidget("Export Folder", parent=self)
         lay.addWidget(self.rootFolder)
         
-        label = QtWidgets.QLabel("Rig Export :", self)
-        lay.addWidget(label)
-        
         layName = QtWidgets.QVBoxLayout(self)
         layName.setContentsMargins(10,0,10,0)
         
@@ -84,9 +83,43 @@ class MyWindow(MayaQWidgetDockableMixin, QtWidgets.QWidget):
         layName.addWidget(self.rigName)
         lay.addLayout(layName)
         
+        label = QtWidgets.QLabel("Rig Export :", self)
+        lay.addWidget(label)
+        
+        self._exportRig = QtWidgets.QCheckBox("Export Rig", self)
+        lay.addWidget(self._exportRig)
+        
         
         self.rigRoot = MayaPickerWidget("Rig Root Object", parent=self)
         lay.addWidget(self.rigRoot)
+        
+        
+        label = QtWidgets.QLabel("Anim Export :", self)
+        lay.addWidget(label)
+        
+        self._exportAnim = QtWidgets.QCheckBox("Export Anim", self)
+        lay.addWidget(self._exportAnim)
+        
+        label = QtWidgets.QLabel("Anim Name :", self)
+        lay.addWidget(label)
+        
+        self._animName = QtWidgets.QLineEdit("", self)
+        lay.addWidget(self._animName)
+        
+        self.animRoot = MayaPickerWidget("Anim Root Object", parent=self)
+        lay.addWidget(self.animRoot)
+        
+        label = QtWidgets.QLabel("start frame :", self)
+        lay.addWidget(label)
+        
+        self._startFrame = QtWidgets.QLineEdit("", self)
+        lay.addWidget(self._startFrame)
+        
+        label = QtWidgets.QLabel("end frame :", self)
+        lay.addWidget(label)
+        
+        self._endFrame = QtWidgets.QLineEdit("", self)
+        lay.addWidget(self._endFrame)
         
         lay.addStretch()
         
@@ -96,8 +129,55 @@ class MyWindow(MayaQWidgetDockableMixin, QtWidgets.QWidget):
         
 
     def onExport(self):
-        print "EXPORT"
+        exportFolder = os.path.normpath ( str( self.rootFolder._line.text() ) )
+        rigRoot = str( self.rigRoot._line.text() )
+        rigName = str( self.rigName.text() )
+        animName = str( self._animName.text() )
+        animRoot = str( self.animRoot._line.text() )
+        startFrame = int( self._startFrame.text() )
+        endFrame = int( self._endFrame.text() )
         
+        print startFrame,  endFrame
+        
+        if exportFolder and rigRoot and rigName and self._exportRig.isChecked() :
+            print "EXPORTING RIG", rigName
+            
+            cmds.select( rigRoot )
+            
+            filePath = os.path.join(exportFolder, rigName+ '.fbx')
+            
+            mel.eval('FBXExportAnimationOnly -v false')       
+            mel.eval('FBXExportBakeComplexAnimation -v true')
+            mel.eval('FBXExportBakeComplexStart -v 0')
+            mel.eval('FBXExportBakeComplexEnd -v 0')
+            mel.eval('FBXExportBakeResampleAnimation -v true')
+            mel.eval('FBXExportConstraints -v false')
+            mel.eval('FBXExportInputConnections -v false')
+            mel.eval('FBXExportSkeletonDefinitions -v true')
+            mel.eval('FBXExportSkins -v true')
+            mel.eval('FBXExport -f "{0}" -s'.format( filePath.replace('\\', '/') ) )
+            
+        else:
+            print "DO NOT EXPORT RIG"
+            
+        
+        if exportFolder and animRoot and rigName and animName and self._exportAnim.isChecked() :
+            print "EXPORTING ANIM", rigName, animName
+            
+            cmds.select( animRoot )
+            
+            filePath = os.path.join(exportFolder, rigName + '@' + animName + '.fbx')
+            
+            mel.eval('FBXExportAnimationOnly -v false')       
+            mel.eval('FBXExportBakeComplexAnimation -v true')
+            mel.eval('FBXExportBakeComplexStart -v {0}'.format(startFrame))
+            mel.eval('FBXExportBakeComplexEnd -v {0}'.format(endFrame))
+            mel.eval('FBXExportBakeResampleAnimation -v true')
+            mel.eval('FBXExportConstraints -v false')
+            mel.eval('FBXExportInputConnections -v false')
+            mel.eval('FBXExportSkeletonDefinitions -v true')
+            mel.eval('FBXExportSkins -v true')
+            mel.eval('FBXExport -f "{0}" -s'.format( filePath.replace('\\', '/') ) )
         
 
         
